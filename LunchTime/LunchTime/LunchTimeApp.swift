06d0f4196de0c5628cf3f0545ec2e773552f99a6
Devin Eror <devineror@gmail.com>
2023-11-14 16:11:37 -0800

    Project Initialized

---
+++ LunchTime/LunchTime/LunchTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LunchTimeApp.swift
+//  LunchTime
+//
+//  Created by Devin Eror on 11/14/23.
+//
+
+import SwiftUI
+
+@main
+struct LunchTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
